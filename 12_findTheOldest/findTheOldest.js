38e91b0480ec0a4deae43d4cc1a44bad923c6f6f
b0llull0s <panicthreads@proton.me>
2024-09-11 16:32:06 +0100

    Done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = (people) => {
+    const currentYear = new Date().getFullYear();
+    
+    return people.reduce((oldest, person) => {
+      const deathYear = person.yearOfDeath || currentYear;
+      const age = deathYear - person.yearOfBirth;
+      
+      if (!oldest || age > (oldest.deathYear - oldest.yearOfBirth)) {
+        return { ...person, deathYear };
+      }
+      return oldest;
+    }, null);
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
