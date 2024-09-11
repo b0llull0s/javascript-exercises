42ad14655586c5ee97864b0cb57075978c4369d4
b0llull0s <panicthreads@proton.me>
2024-09-11 14:07:15 +0100

    Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+// Normalize the string by converting to lowercase and removing non-alphanumeric characters
+  const normalizedStr = str.toLowerCase().replace(/[^a-z0-9]/gi, '');
+  const reversedStr = normalizedStr.split('').reverse().join('');
+  return normalizedStr === reversedStr;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
