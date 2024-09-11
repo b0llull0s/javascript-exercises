2809c3de7afae8b1e5f38f5b01226d205152a3b7
b0llull0s <panicthreads@proton.me>
2024-09-11 15:29:52 +0100

    Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+// Input Handleling
+    if (typeof n === 'string') {
+        n = parseInt(n);
+    }
+    if (n === 0) return 0;
+    if (n < 0) return "OOPS";
+
+
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 
 };
 
