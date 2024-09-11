519414d5216303b8e9e44af8ce5123d451538cb4
b0llull0s <panicthreads@proton.me>
2024-09-11 13:42:35 +0100

    Multiply/Power Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -23,8 +23,9 @@ const power = function(a,b) {
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
